7e9bab980d6c337e3a481257a9c8cbb00e8dc5ee
inpsyde-deploybot <codeship-deployment@inpsyde.com>
2024-01-31 00:02:30 +0000

    Update Gutenberg versions

---
+++ versions/17.5.3/build/preferences/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-a11y', 'wp-components', 'wp-data', 'wp-deprecated', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'd8677261fdc25a957d8d');
---
+++ versions/17.5.3/build/private-apis/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '69e03dbe3168430b6cc3');
