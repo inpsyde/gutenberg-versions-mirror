1713872244082135dda62a1f8ba249c796d9303d
inpsyde-deploybot <codeship-deployment@inpsyde.com>
2024-06-09 00:03:01 +0000

    Update Gutenberg versions

---
+++ versions/18.5.0/build/list-reusable-blocks/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-blob', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '932785631def142b41d9');
