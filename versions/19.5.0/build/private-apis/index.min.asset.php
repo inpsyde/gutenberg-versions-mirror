a7cf45490eefcdcfb00760270a135fab1a6f5c14
inpsyde-deploybot <github-deploybot@inpsyde.com>
2024-11-03 00:03:20 +0000

    Update Gutenberg versions

---
+++ versions/19.5.0/build/private-apis/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'f9dfa9f5ad9f1bbce319');
---
+++ versions/19.5.0/build/router/index.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-polyfill', 'wp-private-apis', 'wp-url'), 'version' => 'f8ae2a3eef0cd387a454');
